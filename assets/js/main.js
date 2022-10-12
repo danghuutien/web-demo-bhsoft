9e393fae94b51568d2d89a2293e375b028d86254
danghuutien <danghuutientk123@gmail.com>
2022-10-12 14:44:03 +0700

    animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,36 +2,28 @@ const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
 const elements =  $$('.section-item_7-content .item')
-// console.log(elements)
+
 
 elements.forEach((element, index)=>{
     // console.log(element)
     element.onclick  = function(){
-        if($('.item-text.animation')){
-            if(element !== $('.item-text.animation')){
-                $('.item-text').classList.add('hidden')
-                $('.item-text.animation').classList.remove('animation')
-                element.querySelector('.item-text').classList.add('animation')
-                console.log('1')
-            }else{
-                if(element.querySelector('.item-text').matches('.hidden')){
-                    element.querySelector('.item-text').classList.remove('hidden')
-                    element.querySelector('.item-text').classList.add('animation')
-                    console.log('rrrrr')
-                }else{
-                    element.querySelector('.item-text').classList.add('hidden')
-                    element.querySelector('.item-text').classList.remove('animation')
-                    console.log('345678')
-                }
-            }
+        if(element.querySelector('.item-text').matches('.animation')){
+            
+            element.querySelector('.item-text.animation').classList.remove('animation')
         }else{
+            if($('.item-text.animation')){
+                
+                $('.item-text.animation').classList.remove('animation')
+            }
+            
             element.querySelector('.item-text').classList.add('animation')
-            element.querySelector('.item-text').classList.remove('hidden')
+        
+            
         }
-        // $('.item-text.animation').classList.remove('animation')
- 
     }
 })
+
+
 
 
 const navbar = document.querySelector(".menu-pc .section-menu");
